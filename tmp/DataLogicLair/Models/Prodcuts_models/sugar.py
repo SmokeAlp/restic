401115850b18fd8cfc7ce21bfd25961c653d3f97
aut1st228pro <eskender0502007@gmail.com>
2023-04-12 22:29:57 +0300

    venv2

--- tmp/DataLogicLair/Models/Prodcuts_models/sugar.py
+++
@@ -1,9 +0,0 @@
-from DataLogicLair.Models.product_input_model import *
-
-
-class Sugar(Product):
-    def __init__(self, amount_for_good):
-        super().__init__(id=10,name='sugar', cost_per_amount=70, unit_of_measurement='kg')
-        # self.amount_in_sklad = None
-        self.amount_for_good = amount_for_good
-
